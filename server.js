d686f91433cb05a032b895af4b2cb8adde155901
Kaishan <kaishanpatel26@gmail.com>
2019-07-23 00:19:31 +0100

    adding port

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.post('/imageurl', (req, res) => { image.handleAPI(req, res) })
 
-app.listen(3000, () => {
-    console.log('App is working');
+app.listen(process.envPORT || 3000, () => {
+    console.log(`App is running on port ${process.env.PORT}`);
 });
